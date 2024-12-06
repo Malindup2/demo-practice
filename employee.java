77878a7dc62b557a32bd954d81b2ad9492515670
Malindu Pabasara <139533700+Malindup2@users.noreply.github.com>
2024-12-06 16:15:25 +0530

    new class file

---
+++ employee.java
@@ -0,0 +1,23 @@
+public class employee {
+    private String name;
+    private int age;
+    private String designation;
+    private double salary;
+
+    public employee(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setDesignation(String designation){
+        this.designation = designation;
+    }    
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+
+}
